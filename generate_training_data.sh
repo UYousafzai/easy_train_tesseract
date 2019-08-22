99095f2ee49457ddbcb53e6ce96f525e24917e73
UYousafzai <umaryousafzai9@gmail.com>
2019-08-22 14:47:59 +0500

    added tess_data and fixed data generation tested

--- generate_training_data.sh
+++ generate_training_data.sh
@@ -1,10 +1,13 @@
-rm -rf train/*
-tesstrain.sh --fonts_dir fonts \
-	     --fontlist 'Agency FB Condensed' \
+rm -rf data/output/*
+tesstrain.sh --fonts_dir data/fonts \
+	     --fontlist 'enter font name here' \
 	     --lang eng \
 	     --linedata_only \
-	     --langdata_dir langdata_lstm \
-	     --tessdata_dir ../tesseract-4.0.0/tessdata \
+	     --langdata_dir data/lang_data \
+	     --tessdata_dir data/tess_data \
 	     --save_box_tiff \
 	     --maxpages 10 \
-	     --output_dir train
+	     --output_dir data/output
+
+#the max pages will enter the amount of pages to be rendered for training purpose
+
