d6049ba5fa4fae278fc36f000b98a2a1b787b09e
UYousafzai <umaryousafzai9@gmail.com>
2019-08-22 12:23:29 +0500

    inital commit for customdata training

---
+++ data_generator.sh
@@ -0,0 +1,10 @@
+rm -rf data/training/*
+tesstrain.sh --fonts_dir fonts \
+	     --fontlist 'Agency FB Condensed' \
+	     --lang eng \
+	     --linedata_only \
+	     --langdata_dir langdata_lstm \
+	     --tessdata_dir ../tesseract-4.0.0/tessdata \
+	     --save_box_tiff \
+	     --maxpages 10 \
+	     --output_dir train
